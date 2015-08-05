b7a95294de5def543f01f367eead432766aa0c33
Mick Rippon <me@mickrippon.com>
2015-08-06 08:36:40 +1000

    User method now supports parameters
    Method wasn't effective if org had >100 users.

--- lib/Bugherd/Api/User.php
+++ lib/Bugherd/Api/User.php
@@ -18,10 +18,11 @@ class User extends AbstractApi
      * List all users
      *
      * @return array list of users found
+     * @param  array $params the additional parameters (cf avaiable $params above)
      */
-    public function all()
+    public function all(array $params = array())
     {
-        $this->users = $this->retrieveAll('/users.json');
+        $this->users = $this->retrieveAll('/users.json', $params);
 
         return $this->users;
     }
